9b8921069f42e118e81f181a76f0efb651bfd483
Michael Kuron <m.kuron@gmx.de>
2021-10-20 22:02:54 +0200

    improvements

--- post_install.sh
+++ post_install.sh
@@ -2,3 +2,5 @@
 
 echo 'tvheadend_enable=YES' >> /etc/rc.conf
 pw usermod tvheadend -G webcamd
+echo 'redir_enable=YES' >> /etc/rc.conf
+echo 'redir_flags="--lport=80 --cport=9981"' >> /etc/rc.conf
--- webcamd.sh
+++ webcamd.sh
@@ -5,10 +5,14 @@ kldload cuse
 
 DEV_NAME="Microsoft Corp. Xbox USB Tuner"
 UGEN_DEV=$(usbconfig | grep "$DEV_NAME" | cut -d':' -f 1)
-LD_LIBRARY_PATH=$PWD nice -n -10 ./webcamd -d $UGEN_DEV -B
+if [ "$UGEN_DEV" != "" ]; then
+	LD_LIBRARY_PATH=$PWD nice -n -10 ./webcamd -d $UGEN_DEV -B
+fi
 
 DEV_NAME="AVerMedia TD310 Device"
 UGEN_DEV=$(usbconfig | grep "$DEV_NAME" | cut -d':' -f 1)
-LD_LIBRARY_PATH=$PWD nice -n -10 ./webcamd -d $UGEN_DEV -B
+if [ "$UGEN_DEV" != "" ]; then
+	LD_LIBRARY_PATH=$PWD nice -n -10 ./webcamd -d $UGEN_DEV -B
+fi
 
 ./rules.sh
